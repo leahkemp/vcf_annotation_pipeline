06c78c5510895f37c46752260af4036e3aba7733
Leah Kemp <leah.kemp@esr.cri.nz>
2020-05-15 08:38:07 +1200

    Minor

--- report.sh
+++ report.sh
@@ -2,5 +2,5 @@
 
 snakemake \
 --report report.html \
---configfile ./config/GRCh38_single_WES_benchmarking_config.yml
+--configfile ./config/GRCh38_single_WES_benchmarking_config.yml \
 --report-stylesheet ESR_stylesheet.css
